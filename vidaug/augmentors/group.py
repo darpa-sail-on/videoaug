33b2037370ed4140553d68d570b320dc94c03cc9
ameya.shringi <ameya.shringi@kitware.com>
2021-04-26 12:43:32 -0400

    Add hashes to group operators

--- vidaug/augmentors/group.py
+++ vidaug/augmentors/group.py
@@ -20,6 +20,25 @@ List of augmenters:
 import numpy as np
 import PIL
 import random
+from typing import Any, Union
+
+
+def _get_transform_hash(transform: Any) -> Union[int, None]:
+    """
+    Private function to get transform for a function.
+
+    Args:
+        transform: Augmentation object
+
+    Return:
+        hash of an object or None if the has does not exist
+    """
+    if hasattr(transform, "hash"):
+        transform_hash = transform.hash
+    else:
+        transform_hash = hash(transform)
+    return transform_hash
+
 
 
 class Sequential(object):
@@ -35,17 +54,25 @@ class Sequential(object):
     def __init__(self, transforms, random_order=False):
         self.transforms = transforms
         self.rand = random_order
+        self.hash = None
 
     def __call__(self, clip):
+        seq_hash = []
         if self.rand:
             rand_transforms = self.transforms[:]
             random.shuffle(rand_transforms)
             for t in rand_transforms:
+                transform_hash = _get_transform_hash(t)
+                if transform_hash is not None:
+                    seq_hash.append(str(transform_hash))
                 clip = t(clip)
         else:
             for t in self.transforms:
+                transform_hash = _get_transform_hash(t)
+                if transform_hash is not None:
+                    seq_hash.append(str(transform_hash))
                 clip = t(clip)
-
+        self.hash = "_".join(seq_hash)
         return clip
 
 
@@ -59,9 +86,11 @@ class OneOf(object):
 
     def __init__(self, transforms):
         self.transforms = transforms
+        self.hash = None
 
     def __call__(self, clip):
         select = random.choice(self.transforms)
+        self.hash = _get_transform_hash(select)
         clip = select(clip)
         return clip
 
@@ -86,23 +115,31 @@ class SomeOf(object):
             raise TypeError('The number of applied augmentors should be smaller than the given augmentation number')
         else:
             self.N = N
+        self.hash = None
 
     def __call__(self, clip):
+        seq_hash = []
         if self.rand:
             indices = [i for i in range(len(self.transforms))]
             selected_indices = [indices.pop(random.randrange(len(indices))) for _ in range(self.N)]
             selected_trans = [self.transforms[i] for i in selected_indices]
             for t in selected_trans:
+                transform_hash = _get_transform_hash(t)
+                if transform_hash is not None:
+                    seq_hash.append(str(transform_hash))
                 clip = t(clip)
-            return clip, selected_indices 
         else:
             indices = [i for i in range(len(self.transforms))]
             selected_indices = [indices.pop(random.randrange(len(indices))) for _ in range(self.N)]
             selected_indices.sort()
             selected_trans = [self.transforms[i] for i in selected_indices]
             for t in selected_trans:
+                transform_hash = _get_transform_hash(t)
+                if transform_hash is not None:
+                    seq_hash.append(str(transform_hash))
                 clip = t(clip)
-            return clip, selected_indices
+        self.hash = "_".join(seq_hash)
+        return clip
 
 
 class Sometimes(object):
@@ -121,13 +158,18 @@ class Sometimes(object):
 
     def __init__(self, p, transform):
         self.transform = transform
+        self.transform_hash = _get_transform_hash(self.transform)
         if (p > 1.0) | (p < 0.0):
             raise TypeError('Expected p to be in [0.0 <= 1.0], ' +
                             'but got p = {0}'.format(p))
         else:
             self.p = p
+        self.hash = None
 
     def __call__(self, clip):
         if random.random() < self.p:
             clip = self.transform(clip)
+            self.hash = self.transform_hash
+        else:
+            self.hash = None
         return clip
